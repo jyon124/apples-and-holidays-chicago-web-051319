24b667782ba7ef1866c07d91c0630c21d01205ac
Jooyon Won <jyon1204@gmail.com>
2019-05-07 23:00:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -8,7 +8,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
       value.each do |food|
         if food == "BBQ"
           empty_cart << food
-          stringed = empty_cart.to_s(0)
+          stringed = empty_cart.to_s
           return stringed
         end
       end
