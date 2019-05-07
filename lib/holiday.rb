5a99fc61927fd98dc4916facbbc54674e535267f
Jooyon Won <jyon1204@gmail.com>
2019-05-07 23:55:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  holiday_hash[:summer][:fourth_of_july][1]
+  puts holiday_hash[:summer][:fourth_of_july][1]
   end
 
   # given that holiday_hash looks like this:
